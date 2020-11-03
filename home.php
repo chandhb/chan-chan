72a4eef560872183ed962f74bb653197b7c1a7d0
Bao Chan <chandhb@propolife.co.jp>
2020-11-03 16:02:44 +0700

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,23 @@
 
 	<?php 
 
-		if ( )
+		if ( have_posts() ) {
+
+			while ( have_posts() ) {
+
+				the_post();
+
+				?>
+
+				<div>
+					<?php echo get_the_title(); ?>
+				</div>
+
+				<?php 
+
+			}
+
+		}
 
 	 ?>
 
