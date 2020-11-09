31746e8ab951f90e69daddc8b3a9e196f85e4cc7
Bao Chan <chandhb@propolife.co.jp>
2020-11-09 09:28:42 +0700

    Update single template

--- single.php
+++ single.php
@@ -1,5 +1,23 @@
 <?php get_header() ?>
 
-	<?php the_title( $before = '', $after = '', $echo = true ) ?>
+	<main class="single-design">
+		
+		<div class="container">
+			
+			<div class="row single-post-content">
+				
+				<h1><?php the_title( $before = '', $after = '', $echo = true ) ?></h1>
+
+				<div class="content">
+					<?php the_content(); ?>
+				</div>
+
+			</div>
+
+		</div>
+
+	</main>
+
+	
 
 <?php get_footer() ?>
